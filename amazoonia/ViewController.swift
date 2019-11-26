42ab80a0d89b83ada5861f901c096bd2b0e71d47
Daniel Martínez Rosillo <daniporre@hotmail.es>
2019-11-26 18:41:54 +0100

    Initial Commit

---
+++ amazoonia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  amazoonia
+//
+//  Created by Daniel Martinez on 26/11/2019.
+//  Copyright © 2019 Amazoonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
